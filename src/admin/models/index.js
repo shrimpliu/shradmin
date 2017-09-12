dcbae6bc9eb9a5f3ce8e5e80be94c95a52718949
shrimp <amoy0226@gmail.com>
2017-09-12 20:41:39 +0800

    加载state

---
+++ src/admin/models/index.js
@@ -0,0 +1,5 @@
+import loadingModel from './loading';
+
+export default [
+  loadingModel,
+];
---
+++ src/admin/models/loading.js
@@ -0,0 +1,9 @@
+export default {
+  name: "loading",
+  initialState: false,
+  reducers: {
+    set(state, data) {
+      return data;
+    }
+  }
+}
