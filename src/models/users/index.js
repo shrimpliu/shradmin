72e3722e513a7b9b927cf20d8d1c02dfe54b968e
shrimp <amoy0226@gmail.com>
2017-09-19 16:06:36 +0800

    创建示例

--- src/models/users/index.js
+++ src/models/users/index.js
@@ -1,9 +1,11 @@
 import UserList from './List';
 import UserIcon from './Icon';
 import UserShow from './Show';
+import UserCreate from './Create';
 
 export {
   UserList,
   UserIcon,
   UserShow,
+  UserCreate,
 };
