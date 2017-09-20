f07780c8932f1052a5d5a3092d132f1e617e39dd
shrimp <amoy0226@gmail.com>
2017-09-20 21:00:48 +0800

    render函数改为format

--- src/admin/components/fields/TextField.js
+++ src/admin/components/fields/TextField.js
@@ -2,18 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import get from 'lodash/get';
 
-const TextField = ({ source, record, render }) => (
-  <span>{render(get(record, source), record)}</span>
+const TextField = ({ source, record, format }) => (
+  <span>{format(get(record, source), record)}</span>
 );
 
 TextField.defaultProps = {
-  render: (value, record) => value
+  format: (value, record) => value
 };
 
 TextField.propTypes = {
   source: PropTypes.string.isRequired,
   record: PropTypes.object,
-  render: PropTypes.func,
+  format: PropTypes.func,
 };
 
 export default TextField;
