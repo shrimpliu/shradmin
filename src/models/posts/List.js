1c27e8b4a68531f3c7eea8ad1501b857102b6f00
shrimp <amoy0226@gmail.com>
2017-09-20 21:40:37 +0800

    多对一关联示例

--- src/models/posts/List.js
+++ src/models/posts/List.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import truncate from 'lodash/truncate';
 import { Link } from 'mirrorx';
-import { List, DataTable, TextField } from '../../admin';
+import { List, DataTable, TextField, BelongsToField } from '../../admin';
 import Filters from './Filters';
 
 export default (props) => (
   <List model="posts" filters={<Filters />} {...props}>
     <DataTable>
       <TextField source="id" />
-      <TextField source="userId" sorter={true} />
-      <TextField source="title" render={(value, record) => <Link to={`/posts/${record.id}/show`}>{value}</Link>} />
-      <TextField source="body" render={value => truncate(value)} />
+      <BelongsToField source="userId" related="users">
+        <TextField source="name"/>
+      </BelongsToField>
+      <TextField source="title" format={(value, record) => <Link to={`/posts/${record.id}/show`}>{value}</Link>} />
+      <TextField source="body" format={value => truncate(value)} />
     </DataTable>
   </List>
 )
