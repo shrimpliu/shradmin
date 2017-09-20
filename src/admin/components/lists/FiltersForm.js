0ac0fe3cab05b151992735acb5617eb3639e1b28
shrimp <amoy0226@gmail.com>
2017-09-20 17:45:40 +0800

    表单字段支持format和parse

--- src/admin/components/lists/FiltersForm.js
+++ src/admin/components/lists/FiltersForm.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Form, Row, Col } from 'antd';
 import mapValues from 'lodash/mapValues';
+import get from 'lodash/get';
 import omitBy from 'lodash/omitBy';
 import isNumber from 'lodash/isNumber';
 import isEmpty from 'lodash/isEmpty';
+import inflection from 'inflection';
 const FormItem = Form.Item;
 
 class FiltersForm extends Component {
@@ -17,7 +19,13 @@ class FiltersForm extends Component {
         <Row gutter={16}>
           {React.Children.map(children, ({ props: { source, input, options } }, index) => (
             <Col key={index} span={6}>
-              <FormItem labelCol={{ span: 6 }} wrapperCol={{ span: 18 }} label={translate(`models.${model}.fields.${source}`)} colon={false}>
+              <FormItem
+                labelCol={{ span: 6 }}
+                wrapperCol={{ span: 18 }}
+                label={translate(`models.${model}.fields.${source}`, {
+                  _: inflection.humanize(source)
+                })}
+                colon={false}>
                 {getFieldDecorator(source, options)(input)}
               </FormItem>
             </Col>
@@ -30,9 +38,35 @@ class FiltersForm extends Component {
 
 export default Form.create({
   mapPropsToFields(props) {
-    return mapValues(props.values, value => ({ value }));
+
+    const formats = {};
+
+    React.Children.forEach(props.children, ({ props: { source, format }}) => {
+      formats[source] = format;
+    });
+
+    return mapValues(props.values, (value, source) => {
+
+      const format = get(formats, source);
+
+      return {
+        value: format ? format(value) : value,
+      };
+    });
   },
   onValuesChange(props, values) {
+
+    const parses = {};
+
+    React.Children.forEach(props.children, ({ props: { source, parse }}) => {
+      parses[source] = parse;
+    });
+
+    values = mapValues(values, (value, source) => {
+      const parse = get(parses, source);
+      return parse ? parse(value) : value;
+    });
+
     props.setFilters(omitBy(values, value => isEmpty(value) && !isNumber(value) ));
   },
 })(FiltersForm);
