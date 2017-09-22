59c9aafc6d074784e0776181fe94442d9f230da5
Kuntong <1161709455@qq.com>
2017-09-22 17:22:48 +0800

    修改后台标题

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import {
   PostList, PostIcon, PostShow,
 } from './models';
 import authClient from './authClient';
-import restClient from './restClient';
+import restClient from './rest';
 import messages from './i18n';
+
 
 class App extends Component {
   render() {
     return (
-      <Admin authClient={authClient} restClient={restClient} language="zh" messages={messages}>
+      <Admin authClient={authClient} title={process.env.REACT_APP_NAME} restClient={restClient} language="zh" messages={messages}>
         <Model name="users" list={UserList} show={UserShow} create={UserCreate} icon={UserIcon}/>
         <Model name="posts" list={PostList} show={PostShow} icon={PostIcon}/>
       </Admin>
