8d81feac95443de1ea976f3b75d1d556203ec509
shrimp <amoy0226@gmail.com>
2017-09-25 22:56:18 +0800

    修复重复定义问题

--- src/components/detail/index.js
+++ src/components/detail/index.js
@@ -2,14 +2,10 @@ import Show from './Show';
 import SimpleShowLayout from './SimpleShowLayout';
 import Create from './Create';
 import Edit from './Edit';
-import SimpleForm from '../form/SimpleForm';
-import FormField from '../form/FormField';
 
 export {
   Show,
   SimpleShowLayout,
   Create,
   Edit,
-  SimpleForm,
-  FormField,
 };
